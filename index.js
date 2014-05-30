17d64fd3c82788a40de4cf4470c80813eaa94134
kirk7880 <kirk7880@gmail.com>
2014-05-30 17:51:54 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var delta = require('delta');
+var time = require('time');
+var followers = delta.create({name: 'followers', time: time.week());
---
+++ time.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var day = (60 * 60 * 24 * 1000);
+var week = (day * 7);
+var month = (week * 4);
+var year = (month * 12);
+
+module.exports = {
+	day: function() {
+		return (new Date().getTime() + day);
+	}
+	,week: function() {
+		return (new Date().getTime() + week);
+	}
+	,month: function() {
+		return (new Date().getTime() + month);
+	}
+	,year: function() {
+		return (new Date().getTime() + year);
+	}
+	,daysAgo: function(num) {
+		return (new Date().getTime() - (num * days));
+	}
+	,weeksAgo: function(num) {
+		return (new Date().getTime() - (num * week));
+	}
+	,monthsAgo: function(num) {
+		return (new Date().getTime() - (num * week));
+	}
+	,yearsAgo: function(num) {
+		return (new Date().getTime() - (num * year));
+	}
+};
