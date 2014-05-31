113900afa322f7bb90f59c1fa8573aca63fdd26c
kirk7880 <kirk7880@gmail.com>
2014-05-31 11:09:55 -0400

    Updated indentation

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ var time = require('./time');
 var when = require('when');
 
 when(delta.create({
-	name: 'followers'
-	,time: time.week()
+  name: 'followers'
+  ,time: time.week()
 })).then(function(delta) {
-	console.log('res', delta);
-	//delta.incr();
+  console.log('res', delta);
+  //delta.incr();
 }).otherwise(function(e) {
-	console.log('error', e);
+  console.log('error', e);
 });
-
-//var followers = delta.create({name: 'followers', time: time.week()});
