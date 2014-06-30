6f5212e6049a8e60dea006454230259f7ce34d70
kirk7880 <kirk7880@gmail.com>
2014-06-30 13:56:34 -0400

    Fixed connection

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var path = require('path');
-exports.Delta = require(path.resolve(__dirname + '/delta'));
-exports.time = require(path.resolve(__dirname + '/time'));
+exports.Delta = require(path.resolve(__dirname + '/lib/delta'));
+exports.time = require(path.resolve(__dirname + '/lib/time'));
