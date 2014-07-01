f9f3933b3faca4c90383e2e20c0a6f160b7901df
kirk7880 <kirk7880@gmail.com>
2014-07-01 05:50:39 -0400

    Fixed date bug

--- test.js
+++ test.js
@@ -1,24 +1,41 @@
 var Delta = require(__dirname + '/lib/delta');
 var moment = require('moment');
 
-var category = 'shares';
-var bin = 'trend';
+var delta = 'shares';
+var bin = 'my-content-id';
 
-Delta.create({
-  name: category
-  ,time: moment().week()
-}, function(e, delta) {
-  delta.foo = bin
-  console.log(e);
-  // increment a new bin
-  delta.incr({
-    bin: bin
-    ,by: 1
-  }, function(e) {
-    // fetch all item
-    delta.fetch({
-    }, function(e, trends) {
-      console.log('trends', trends);
+function getDays(days) {
+  return (new Date().getTime() + ((60 * 60 * 24 * 1000) * days));
+}
+
+function createAndIncrement(cb) {
+  Delta.create({
+    name: delta
+    ,time: getDays(7)
+  }, function(e, delta) {
+    if (e) return console.log('Error creating delta', e);
+
+    // increment a new bin
+    delta.incr({
+      bin: bin
+      ,by: 1
+    }, function(e) {
+      if (e) return console.log('Error incrementing bin', e);
+      cb();
+    })
+  });
+}
+
+function fetch() {
+  Delta.get(delta, function(e, delta) {
+    if (e) return console.log('Delta does not exists', e);
+
+    delta.fetch({date: getDays(1)}, function(e, trends) {
+      if (e) return console.log('Error fetching all trends in ' + delta);
+      console.log('Trends', trends);
     })
   })
-});
+}
+
+createAndIncrement(fetch);
+//fetch();
