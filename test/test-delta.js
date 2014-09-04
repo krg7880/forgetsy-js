69f58add61e1aba64ab0cf22cdc68f0c0b76fc20
kirk7880@gmail.com <kirk7880@gmail.com>
2014-09-04 10:22:38 -0400

    Promisifying set and delta

--- test/test-delta.js
+++ test/test-delta.js
@@ -10,46 +10,49 @@ var expect = chai.expect;
 var client = redis.createClient("", "", "");
 Delta.setRedisClient(client);
 
+var dist = 'people_camping';
+
 function getDays(days) {
   return (new Date().getTime() + ((60 * 60 * 24 * 1000) * days));
 }
 
 describe('testing delta', function() {
-	it('should be a valid delta instance', function(done) {
-		Delta.create({
-	    name: 'dist'
-	    ,time: getDays(14)
-	  })
-	  .then(function onCreateComplete(delta) {
-	  	done();
-	  })
-	  .catch(function onCreateError(e) {
-	  	console.log(e)
-	  })
-	});
+  it('should be a valid delta instance', function(done) {
+    Delta.create({
+      name: dist
+      ,time: getDays(14)
+    })
+    .then(function onCreateComplete(delta) {
+      done();
+    })
+    .catch(function onCreateError(e) {
+      console.log(e)
+    })
+  });
 
-	it('should return a valid dist', function(done) {
-		Delta.get('dist')
-			.then(function onGetComplete(delta) {
-				done();
-			})
-			.catch(function onGetError(e) {
-				console.log(e);
-			})
-	})
+  it('should return a valid dist', function(done) {
+    Delta.get(dist)
+      .then(function onGetComplete(delta) {
+        done();
+      })
+      .catch(function onGetError(e) {
+        console.log(e);
+      })
+  })
 
-	it('should return a valid dist set', function(done) {
-		Delta.get('dist')
-			.then(function onGetComplete(delta) {
-				delta.fetch().then(function() {
-					done()
-				})
-				.catch(function(e) {
-					console.log('Fetch error');
-				})
-			})
-			.catch(function onGetError(e) {
-				console.log(e);
-			})
-	})
+  it('should return a valid dist set', function(done) {
+    Delta.get(dist)
+      .then(function onGetComplete(delta) {
+        delta.fetch().then(function(results) {
+          console.log('results', results);
+          done()
+        })
+        .catch(function(e) {
+          console.log('Fetch error');
+        })
+      })
+      .catch(function onGetError(e) {
+        console.log(e);
+      })
+  })
 });
